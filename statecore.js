47f838d5d7b2554605cde2d68e2616f8bbb21cb3
Zen <zenyes@gmail.com>
2020-10-30 22:02:31 +1300

    feat: 🎸 (discardError)

--- statecore.js
+++ statecore.js
@@ -7,17 +7,16 @@
   return { createStateCore: function createStateCore(state) {
     var undefined = (function _undefined() {})();
     var allObservers = [];
-    var errorDiscarded = new Error('This StateCore has been discarded!');
     function discard() { state = undefined; allObservers = undefined; }
     function isDiscarded() { return allObservers === undefined; }
     function getState() { return state; }
     function setState(newState) {
-      if (isDiscarded()) throw errorDiscarded;
+      if (isDiscarded()) throw new Error('This StateCore has been discarded!');
       state = newState;
       return state;
     }
     function addObserver(observer) {
-      if (isDiscarded()) throw errorDiscarded;
+      if (isDiscarded()) throw new Error('This StateCore has been discarded!');
       allObservers.push(observer);
       return function removeObserver() {
         if (allObservers && observer) allObservers.splice(allObservers.indexOf(observer), 1);
@@ -25,7 +24,7 @@
       };
     }
     function notifyAllObservers() {
-      if (isDiscarded()) throw errorDiscarded;
+      if (isDiscarded()) throw new Error('This StateCore has been discarded!');
       for (var idx = 0; idx < allObservers.length; idx += 1) allObservers[idx].apply(this, arguments);
     }
     return { getState: getState, setState: setState, addObserver: addObserver, notifyAllObservers: notifyAllObservers, discard: discard };
