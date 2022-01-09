ab3cb9142d12dab3d694ef0f46b9917b1f1f9245
MrZenW <62133866+MrZenW@users.noreply.github.com>
2022-01-10 01:44:52 +1300

    optimise

--- statecore.js
+++ statecore.js
@@ -1,21 +1,28 @@
 (function moduleify(moduleFactory) {
   'use strict';
+  var statecoreLib = null;
   if (typeof define === 'function' && define.amd) {
-    define('statecore', [], moduleFactory);
-    define('StateCore', [], moduleFactory);
+    var amdFactory = function () {
+      statecoreLib = statecoreLib || moduleFactory.apply(this, arguments);
+      return statecoreLib;
+    });
+    define('statecore', [], amdFactory);
+    define('StateCore', [], amdFactory);
   } else if (typeof module === 'object' && typeof exports === 'object') {
-    module.exports = moduleFactory();
+    statecoreLib = statecoreLib || moduleFactory();
+    module.exports = statecoreLib;
   }
   var root = (typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : typeof global !== 'undefined' ? global : this);
   if (root && typeof root === 'object') {
-    root['statecore'] = moduleFactory();
-    root['StateCore'] = root['statecore'];
+    statecoreLib = statecoreLib || moduleFactory();
+    root['statecore'] = statecoreLib;
+    root['StateCore'] = statecoreLib;
   }
 })(function moduleFactory () {
   'use strict';
   return { createStatecore: function createStatecore(state) {
     var allObservers = [];
-    function statecoreDiscard() { state = null; allObservers.splice(0); allObservers = null; }
+    function statecoreDiscard() { state = null; allObservers = false; }
     function statecoreIsDiscarded() { return !allObservers; }
     function statecoreGetState() { return state; }
     function statecoreSetState(newState) {
@@ -25,16 +32,27 @@
     }
     function statecoreRemoveObserver(observer) {
       if (statecoreIsDiscarded()) return;
+      
+      var newAllObservers = [];
       var copyObservers = allObservers;
-      allObservers = [];
-      for (var idx = 0; idx < copyObservers.length; idx += 1) {
-        if (observer !== copyObservers[idx]) allObservers.push(copyObservers[idx]);
+      var observersLength = copyObservers.length;
+      for (var idx = 0; idx < observersLength; idx += 1) {
+        if (observer !== copyObservers[idx]) newAllObservers.push(copyObservers[idx]);
       }
+      allObservers = newAllObservers;
     }
     function statecoreAddObserver(observer) {
       if (statecoreIsDiscarded()) throw new Error('This Statecore has been discarded!');
       if (typeof observer !== 'function') throw new Error('The observer must be a function!');
-      allObservers.push(observer);
+
+      var newAllObservers = [];
+      var copyObservers = allObservers;
+      var observersLength = copyObservers.length;
+      for (var idx = 0; idx < observersLength; idx += 1) {
+        newAllObservers.push(copyObservers[idx]);
+      }
+      newAllObservers.push(observer);
+      allObservers = newAllObservers;
       return function removeObserver() {
         if (observer) {
           statecoreRemoveObserver(observer);
@@ -45,7 +63,8 @@
     function statecoreNotifyAllObservers() {
       if (statecoreIsDiscarded()) throw new Error('This Statecore has been discarded!');
       var copyObservers = allObservers;
-      for (var copyIdx = 0; copyIdx < copyObservers.length; copyIdx += 1) copyObservers[copyIdx].apply(this, arguments);
+      var observersLength = copyObservers.length;
+      for (var copyIdx = 0; copyIdx < observersLength; copyIdx += 1) copyObservers[copyIdx].apply(this, arguments);
     }
     return { statecoreGetState: statecoreGetState, statecoreSetState: statecoreSetState, statecoreAddObserver: statecoreAddObserver, statecoreRemoveObserver: statecoreRemoveObserver, statecoreNotifyAllObservers: statecoreNotifyAllObservers, statecoreDiscard: statecoreDiscard, statecoreIsDiscarded: statecoreIsDiscarded };
   }};
