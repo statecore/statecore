12fbe5d7ad513c3505e8985315cc2eddd7d8d147
MrZenW <MrZenW@gmail.com>
2024-12-31 17:34:57 +1300

    refine declaration of StatecoreClass in statecore.d.ts

--- statecore.d.ts
+++ statecore.d.ts
@@ -29,6 +29,17 @@ export const STATECORE_EVENT_NAME_STATE: string;
 export function createStatecore(initialState?: any): Statecore;
 
 export class StatecoreClass implements Statecore {
+  // StatecoreClass methods
+  constructor(initialState?: any);
   statecoreClassAddEventObserver(eventName: string, observer: StatecoreObserver): StatecoreObserverRemover;
   statecoreClassNotifyAllEventObservers(eventName: string, ...args: any[]): void;
+  // Statecore interface
+  statecoreDiscard(): void;
+  statecoreIsDiscarded(): boolean;
+  statecoreGetState(): any;
+  statecoreSetState(state: any): any;
+  statecoreRemoveObserver(observer: StatecoreObserver): void;
+  statecoreAddObserver(observer: StatecoreObserver): StatecoreObserverRemover;
+  statecoreGetAllObservers(): StatecoreObserver[] | null;
+  statecoreNotifyAllObservers(...args: any[]): void;
 }
